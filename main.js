62b49228860afb63f2963843b6f80fe1e9f08c44
Yerlin0210 <97493157+Yerlin0210@users.noreply.github.com>
2022-01-26 16:14:48 -0300

    Cambios guardados

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 window.sr = ScrollReveal();
 
-ScrollReveal().reveal('.navbar');
-ScrollReveal().reveal('.card', { delay: 500 });
-ScrollReveal().reveal('.carousel', { delay: 500 });
-ScrollReveal().reveal('.footer-links', { delay: 500 });
+ScrollReveal().reveal('.navbar',{ delay: 500, origin: "bottom", distance: "-50px" });
+
+ScrollReveal().reveal('.card', { delay: 500, origin: "top", distance: "-100px" });
+ScrollReveal().reveal('.cards',{ delay: 500, origin: "top", distance: "-400px" } );
+ScrollReveal().reveal('.carousel', { delay: 500, origin: "Left", distance: "-400px" });
+ScrollReveal().reveal('.footer-links', { delay: 500 ,origin: "bottom",distance: "-300px"});
